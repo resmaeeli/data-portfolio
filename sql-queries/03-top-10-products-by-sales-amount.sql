9894b9d72ea04eab62523c024ee7eff2c0bdf172
Reza Esmaeeli <r.esmaeeli@gmail.com>
2026-06-30 10:24:39 +0330

    Add product sales report query

---
+++ sql-queries/03-top-10-products-by-sales-amount.sql
@@ -0,0 +1,23 @@
+﻿
+
+	--****************************************************
+	-- Title: Top 10 Products by Sales Amount
+	-- By: R.Esmaeeli
+	-- Date: 2026-06-30
+	-- Version : 1.0
+	-- Description: 
+	-- Returns top 10 products by total purchase amount
+	--
+	--****************************************************
+	
+	USE AdventureWorks2022
+	GO
+
+	SELECT TOP 10 
+		sod.ProductID, 
+		pr.Name AS ProductName,
+		SUM(sod.LineTotal) AS TotalSalesAmount
+	FROM Sales.SalesOrderDetail AS sod
+		INNER JOIN Production.Product AS pr on sod.ProductID = pr.ProductID
+	GROUP BY sod.ProductID , pr.Name
+	ORDER BY TotalSalesAmount DESC
