986ffac2159d31e1f63dd9213e47d5761636a49e
Reza Esmaeeli <r.esmaeeli@gmail.com>
2026-07-01 11:26:49 +0330

    Monthly sales growth

---
+++ sql-queries/10-monthly-sales-growth.sql
@@ -0,0 +1,43 @@
+﻿
+
+	--**********************************************************************
+	-- Title: Monthly sales growth
+	-- By: R.Esmaeeli
+	-- Date: 2026-07-01
+	-- Version : 1.0
+	-- Description: 
+	-- Returns sales growth by month
+	-- 
+	--**********************************************************************
+	
+	USE AdventureWorks2022
+	GO
+
+
+	--===========  BEGIN CTE PART ===========--
+
+	WITH MonthlySalesInfo AS(
+	SELECT 
+			YEAR(OrderDate) AS SalesYear,
+			MONTH(OrderDate) AS SalesMonth,
+			SUM(TotalDue) AS TotalSales,
+			LAG(SUM(TotalDue),1,0)
+			OVER (PARTITION BY YEAR(OrderDate) ORDER BY MONTH(OrderDate)) AS PreviousMonthSales				
+		FROM Sales.SalesOrderHeader osh
+		GROUP BY 
+				YEAR(OrderDate) , 
+				MONTH(OrderDate)		
+	)
+	--===========  END CTE PART ===========--
+	SELECT 
+			SalesYear,
+			SalesMonth, 
+			TotalSales,
+			PreviousMonthSales,
+			(TotalSales - PreviousMonthSales) AS GrowthAmount
+
+	FROM MonthlySalesInfo
+
+	ORDER BY 
+		SalesYear,
+		SalesMonth
