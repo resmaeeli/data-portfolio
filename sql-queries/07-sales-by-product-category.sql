517b9ca630f50dc764b8965d57f6d3d6a9b118b6
Reza Esmaeeli <r.esmaeeli@gmail.com>
2026-07-01 08:52:30 +0330

    Add sales by product category

---
+++ sql-queries/07-sales-by-product-category.sql
@@ -0,0 +1,26 @@
+
+
+	--***************************************************************
+	-- Title: Sales by product category
+	-- By: R.Esmaeeli
+	-- Date: 2026-07-01
+	-- Version : 1.0
+	-- Description: 
+	-- Returns the count of purchase and total amount of sales by product category
+	-- 
+	--***************************************************************
+	
+	USE AdventureWorks2022
+	GO
+
+	SELECT 
+		pc.Name AS CategoryName,
+		SUM(sod.OrderQty) AS TotalQuantitySold,		
+		SUM(sod.LineTotal) AS TotalSalesAmount
+		
+	FROM Sales.SalesOrderDetail sod
+		INNER JOIN Production.Product p on sod.ProductID = p.ProductID
+		INNER JOIN Production.ProductSubcategory psc on p.ProductSubcategoryID = psc.ProductSubcategoryID
+		INNER JOIN Production.ProductCategory pc on pc.ProductCategoryID = psc.ProductCategoryID
+
+	GROUP BY pc.ProductCategoryID , pc.Name
