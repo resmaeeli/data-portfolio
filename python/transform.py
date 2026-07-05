b1e43401d0149d0efd0280fe9d598520d47d42e9
Reza Esmaeeli <r.esmaeeli@gmail.com>
2026-07-05 14:21:17 +0330

    Add second export using generic ETL pipeline

--- python/transform.py
+++ python/transform.py
@@ -10,3 +10,18 @@ def transform_orders_per_year(rows):
         result.append({"Year": year, "Count": count})
 
     return result
+
+
+def transform_top_10_customers(rows):
+    result = []
+
+    for StoreName, PersonName, TotalPurchasedAmount in rows:
+        result.append(
+            {
+                "StoreName": StoreName,
+                "PersonName": PersonName,
+                "TotalPurchasedAmount": TotalPurchasedAmount,
+            }
+        )
+
+    return result
