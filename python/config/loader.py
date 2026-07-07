a1f89755bad5e2395d7a19361281a80f213753af
Reza Esmaeeli <r.esmaeeli@gmail.com>
2026-07-07 08:54:40 +0330

    Add global application settings support

--- python/config/loader.py
+++ python/config/loader.py
@@ -11,7 +11,18 @@ def load_jobs():
 
     json_file = Path(__file__).parent.parent.parent / "config/export_jobs.json"
 
-    with open(json_file, "r", encoding="utf-8") as file:
+    with open(json_file, "r", encoding="utf-8-sig") as file:
         records = json.load(file)
 
     return records["jobs"]
+
+
+def load_app_settings():
+    """Load items from system configuration file."""
+
+    json_file = Path(__file__).parent.parent.parent / "config/app_settings.json"
+
+    with open(json_file, "r", encoding="utf-8-sig") as file:
+        settings = json.load(file)
+
+    return settings
