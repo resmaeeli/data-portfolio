a91cbc87d0e44760357cf847a28956c04b9b6cc5
Reza Esmaeeli <r.esmaeeli@gmail.com>
2026-07-28 12:44:07 +0330

    feat(sqlserver): add automated database restore script

---
+++ docker/init/sqlserver/restore.sql
@@ -0,0 +1,8 @@
+RESTORE DATABASE AdventureWorks2022
+FROM DISK = '/init/backup/AdventureWorks2022.bak'
+WITH
+    MOVE 'AdventureWorks2022'
+    TO '/var/opt/mssql/data/AdventureWorks2022.mdf',
+    MOVE 'AdventureWorks2022_log'
+    TO '/var/opt/mssql/data/AdventureWorks2022_log.ldf',
+    REPLACE;
