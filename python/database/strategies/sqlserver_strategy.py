91fc33cb71a2a73308d676f8c3b6a504304e7c16
Reza Esmaeeli <r.esmaeeli@gmail.com>
2026-08-23 15:45:50 +0330

    feat: integrate Airflow orchestration with data portfolio export pipeline

--- python/database/strategies/sqlserver_strategy.py
+++ python/database/strategies/sqlserver_strategy.py
@@ -5,13 +5,13 @@ SqlServer connection provider strategy
 import pyodbc
 
 
-
 def _get_odbc_driver():
-    """ finding appropriate odbc driver """
+    """finding appropriate odbc driver"""
 
     drivers = pyodbc.drivers()
     odbc_drivers = [
-        driver for driver in drivers
+        driver
+        for driver in drivers
         if driver.startswith("ODBC Driver") and "SQL Server" in driver
     ]
 
@@ -19,26 +19,26 @@ def _get_odbc_driver():
         raise Exception("No supported SQL Server ODBC driver found.")
 
     odbc_drivers.sort(
-        key=lambda x: int(x.split()[2]), ## sort by int value
-        reverse=True
+        key=lambda x: int(x.split()[2]), reverse=True  ## sort by int value
     )
     return odbc_drivers[0]
 
 
 def _get_dsn(db_config):
-    """ Create DSN """  
+    """Create DSN"""
     dsn = f"""
         DRIVER={{{_get_odbc_driver()}}};
         SERVER={db_config["server"]};
         DATABASE={db_config["database"]};
         UID={db_config["username"]};
         PWD={db_config["password"]};
-        TrustServerCertificate={db_config["trust_server_certificate"]};
+        TrustServerCertificate={db_config["trust_server_certificate"]};        
+        Encrypt={db_config.get("Encrypt", "yes")};
         """
     return dsn
 
 
 def get_connection(db_config):
-    """ Get SQL Server connection """
+    """Get SQL Server connection"""
     connection = pyodbc.connect(_get_dsn(db_config))
     return connection
